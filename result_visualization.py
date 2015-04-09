7c92b9ce45a7dc7c91d1dbb3671cdd6531d03d9d
yingdongo <yingdongo@hotmail.com>
2015-04-09 21:29:03 +0200

    update

--- result_visualization.py
+++ result_visualization.py
@@ -6,9 +6,9 @@ Created on Wed Apr  8 18:34:01 2015
 """
 
 import matplotlib.pyplot as plt
-
-scores=[0.75614,0.75754,0.80845,0.81096,0.81092,0.81944]
 names=["simple","preprocess","feature_engineering","parameter_tuning","feature_selection","algorithm"]
+cv_scores=[0.784656084656,0.783068783069,0.816798941799,0.821428571429,0.821759259259,0]
+scores=[0.75614,0.75754,0.80845,0.81096,0.81092,0.81944]
 rankings=[557,527,105,73,74,24]
 
 fig = plt.figure(figsize=(8,8))
